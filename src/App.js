1d89165af6def0c3325742baac719201279424c0
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2020-01-26 20:37:13 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, } from 'react';
+import axios from "axios"
+import { Header, Button, Segment, Card, Icon, } from 'semantic-ui-react';
+import styled from 'styled-components';
+import HeaderText from "./styles/HeaderText"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <AppContainer>
+    <HeaderText fSize="large">My Portfolio</HeaderText>
+    <Segment as={Transparent}>
+      <HeaderText fSize="small" >My Projects</HeaderText>
+    </Segment>
+    <Segment as={Transparent}>
+      <HeaderText>Contact</HeaderText>
+    </Segment>
+  </AppContainer>
+);
+
+
+
+
+
+
+
+const AppContainer = styled.div`
+  background: linear-gradient(to bottom right, aliceblue, black);
+`;
+
+const Transparent = styled.div`
+  background: transparent !important;
+`;
+
+
 
 export default App;
---
+++ src/styles/HeaderText.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+
+
+const fontSize = (size) => {
+  switch(size) {
+    case 'large':
+      return '40px';
+    case 'small':
+      return '25px';
+    default:
+      return '20px';
+  }
+};
+
+const HeaderText = styled.h1`
+  color: white !important;
+  text-align: center;
+  font-size: ${props => fontSize(props.fSize)} !important;
+`;
+
+export default HeaderText;
